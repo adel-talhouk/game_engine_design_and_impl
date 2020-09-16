5c05f3a4401eeabab7a82b27d71907164a552262
Adel Talhouk <adel.talhouk@mymail.champlain.edu>
2020-09-16 15:40:44 -0400

    Basic setup for mouse_image. Need to implement methods in the class.

---
+++ mouse_image/mouseImageNode.cpp
@@ -0,0 +1,15 @@
+#include "mouseImageNode.h"
+
+CalculatorNode::CalculatorNode(){}
+
+//Bind all your methods used in this class
+void MouseImageNode::_bind_methods(){
+  //ClassDB::bind_method(D_METHOD("add", "a", "b"), &CalculatorNode::add);
+  //ClassDB::bind_method(D_METHOD("sub", "a", "b"), &CalculatorNode::sub);
+  //ClassDB::bind_method(D_METHOD("mul", "a", "b"), &CalculatorNode::mul);
+  //ClassDB::bind_method(D_METHOD("div", "a", "b"), &CalculatorNode::div);
+}
+
+
+//Custom Functions
+
---
+++ mouse_image/mouseImageNode.h
@@ -0,0 +1,21 @@
+//insert the Headername here
+#ifndef MOUSEIMAGENODE_H
+#define MOUSEIMAGENODE_H
+
+#include "scene/main/node.h"
+
+class MouseImageNode : public Node {
+	GDCLASS(MouseImageNode, Node);
+
+protected:
+  static void _bind_methods();
+
+public:
+  MouseImageNode();
+
+  //Methods
+
+
+};
+
+#endif
