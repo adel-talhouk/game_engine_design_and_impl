436aa27459f0972d60113af7ed893c8023979808
Adel Talhouk <adel.talhouk@mymail.champlain.edu>
2020-10-11 17:45:41 -0400

    Added the previous project from my laptop, I didn't notice that it was not added before, as I had it in a folder named Projects, but that was being ignored. Added it to Test_Projects. Saving progress.

---
+++ Test_Projects/Testing_Custom_Nodes/MouseImageNode.gd
@@ -0,0 +1,15 @@
+extends MouseImageNode
+
+
+# Declare member variables here. Examples:
+var theTexture = load("res://zoom.png")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	setTexture(theTexture)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
